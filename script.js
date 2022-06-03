34a665d4047daaffba9cb679ebd3e43e1382e35c
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-06-03 21:09:13 +0900

    0603

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-console.log(1 + 4);
-console.log(2 ** 3);
+//function ask(question, yes, no) {
+//  if (confirm(question)) yes()
+//  else no();
+//}
+
+//let ask = (question, yes, no) => {
+//  if (confirm(question)) yes()
+//  else no();
+//}
+
+//ask(
+//  "Do you agree?",
+//  function () { alert("You agreed."); },
+//  function () { alert("You canceled the execution."); }
+//);
+
+let user = {
+  name: "John",
+  age: 30
+};
+
+let clone = { ...user, sex: "female" }
+
+
+alert(clone.sex);
